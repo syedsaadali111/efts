635f220613c5a5f1aafbb924be96073841155a2d
salahuddin-here <salahuddin.kurd@ug.bilkent.edu.tr>
2020-12-22 01:24:50 +0300

    First commit for verification of citizen from db.

---
+++ server/ms_citizen/citizendb.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+const citizenSchema = mongoose.Schema({
+  TC: Number,
+  FName: String,
+  SName: String,
+  DOB: String,
+  Gender: String,
+});
+
+const Citizens = mongoose.model('citizens', citizenSchema);
+module.exports = Citizens;
+
+/*this is the Schema for MongoDB. The citizens written in the 
+  second last line is the collection(table) in MongoDB.
+*/
