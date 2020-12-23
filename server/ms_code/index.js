e5f2def3832f4b0a37844161ac4a7f9992f9dab6
Daniyal Admaney <daniyal.admany@ug.bilkent.edu.tr>
2020-12-23 17:33:09 +0300

    increased code to 16 characters

--- server/ms_code/index.js
+++ server/ms_code/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const randomstring = require ('randomstring'); 
 var hashSum = require('hash-sum');
+
 //var MongoClient = require('mongodb').MongoClient;
 //var url = "mongodb://localhost:27017/";
 const app = express();
@@ -19,7 +21,7 @@ app.post('/generate', (req, res) => {
     console.log("req body id " + req.body.id);
 
 
-    if (typeof req.body.id !== "number") {
+    if (typeof req.body.id !== "number") {  
         res.status(400).send({ msg: "Failed. Incorrect format for: id" });
         return;
     }
@@ -28,7 +30,7 @@ app.post('/generate', (req, res) => {
         var hashcode = hashSum(req.body.id);
         console.log(hashcode);
         //divide code into 3 sections and add EFTS at the beginning
-        var eftsCode = "EFTS-" + hashcode.slice(0, 4) + "-" + hashcode.slice(3);
+        var eftsCode = "EFTS-" + hashcode.slice(0, 4) + "-" + hashcode.slice(4) + "-" + randomstring.generate(4);
         console.log(eftsCode);
         //var hashcode = hashSum(req.body.id)
         //res.status(200).send(eftsCode);
