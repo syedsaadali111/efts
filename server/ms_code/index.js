e2c87e93e0ea7cd2ebf5ba777cdb0a17e83238fc
Minal Zaka <minalzaka@gmail.com>
2020-12-25 03:48:16 +0300

    added twilio sms + changed 'TTL' to 'ttl' for request

--- server/ms_code/index.js
+++ server/ms_code/index.js
@@ -1,11 +1,19 @@
+require('dotenv').config();
+
+//twilio
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twilioClient = require('twilio')(accountSid, authToken);
+
 const express = require('express');
 const randomstring = require('randomstring');
 var hashSum = require('hash-sum');
 
-Cors = require("cors"); // required for providing connection in the middleware. 
+// required for providing connection in the middleware. 
+const cors = require("cors"); 
 var userModel = require('./user');
 var mongoose = require('mongoose');
-var QRCode      =   require('qrcode');
+var QRCode = require('qrcode');
 
 const connectionURL = "mongodb+srv://admin:admin@efts.zqahh.mongodb.net/EFTS?retryWrites=true&w=majority";
 
@@ -14,10 +22,11 @@ mongoose.connect(connectionURL, { useNewUrlParser: true })
     .then(() => console.log('connected to db'))
     .catch((err) => console.log(err))
 
-//connect exptress
+//connect express
 const app = express();
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded()); 
+app.use(express.urlencoded());
 
 
 //testing with hardcoding
@@ -41,53 +50,72 @@ app.post('/generate', (req, res) => {
     else {
         //create hash from citizen id
         var hashcode = hashSum(req.body.id);
-        console.log(hashcode);
         //divide code into 4 sections and add EFTS at the beginning
         var eftsCode = "EFTS-" + hashcode.slice(0, 4) + "-" + hashcode.slice(4) + "-" + randomstring.generate(4);
-        console.log(eftsCode);
         //var hashcode = hashSum(req.body.id)
         //res.status(200).send(eftsCode);
 
         //send to mongodb
-        QRCode.toDataURL(eftsCode,{errorCorrectionLevel:'H'},function (err, url) {
-        if (err) {
-            res.status(500).send(err);
-        }
-          else {
-            var userr = new userModel({
-                TC:req.body.id,
-                EFTScode:eftsCode,
-                qrcode_image:url
+        QRCode.toDataURL(eftsCode, { errorCorrectionLevel: 'H' }, function (err, url) {
+            if (err) {
+                res.status(400).send(err);
+            }
+            else {
+                var userr = new userModel({
+                    TC: req.body.id,
+                    EFTScode: eftsCode,
+                    qrcode_image: url
                 });
-                userr.expirationDate = new Date(Date.now() + req.body.TTL);
-                userr.save((err,data)=>{
-                    if(err){
-                        console.log(err);
-                    }else{
-                        res.status(200).json({
-                            msg: 'EFTS Code successfully Generated.',
-                            efts: eftsCode,
-                            id: req.body.id,
-                            qrcode:url
+                userr.expirationDate = new Date(Date.now() + req.body.ttl);
+                userr.save((err, data) => {
+                    if (err) {
+                        console.log('THIS ERROR!!')
+                        res.status(400).send(err);
+                    } else {
+                        //sending an sms with the EFTS code to the user
+                        var smsMsg = "\nYour EFTS Code: \n" + eftsCode;
+
+                        twilioClient.messages.create({
+                            body: smsMsg,
+                            from: '+14793483249',
+                            to: '+905454683730',
+                        })
+                        .then((msg) => {
+                            res.status(200).json({
+                                msg: 'EFTS Code successfully generated and sent as an SMS.',
+                                efts: eftsCode,
+                                id: req.body.id,
+                                qrcode: url,
+                                smsMsg: msg
+                            });
+                        })
+                        .catch((err) => {
+                            res.status(200).json({
+                                msg: 'EFTS Code successfully generated and sent as an SMS.',
+                                efts: eftsCode,
+                                id: req.body.id,
+                                qrcode: url,
+                                smsMsg: null
+                            });
                         });
                     }
-                });                                         
-          }
-      });
-    //   console.Console(qrcode);         
-    //     var userr = new userModel({
-    //         TC:req.body.id,
-    //         EFTScode:eftsCode,
-    //         qrcode_image:url
-    //         });
-    //         userr.expirationDate = new Date(Date.now() + req.body.TTL);
-    //         userr.save((err,data)=>{
-    //             if(err){
-    //                 console.log(err);
-    //             }else{
-    //                 console.log("Saved to Database");
-    //             }
-    //         });
+                });
+            }
+        });
+        //   console.Console(qrcode);         
+        //     var userr = new userModel({
+        //         TC:req.body.id,
+        //         EFTScode:eftsCode,
+        //         qrcode_image:url
+        //         });
+        //         userr.expirationDate = new Date(Date.now() + req.body.ttl);
+        //         userr.save((err,data)=>{
+        //             if(err){
+        //                 console.log(err);
+        //             }else{
+        //                 console.log("Saved to Database");
+        //             }
+        //         });
     }
 
 });
