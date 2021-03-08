229942e5c15434b516ad5ac8a00ba0dc377a348e
Syed Saad Ali <saad.ali@ug.bilkent.edu.tr>
2021-03-08 03:43:44 +0300

    Create Rule Page Modifications

--- client_pi/src/pages/CreateRule/CreateRule.jsx
+++ client_pi/src/pages/CreateRule/CreateRule.jsx
@@ -65,6 +65,8 @@ const CreateRule = () => {
             endDate: "",
             minAge: "",
             maxAge: "",
+            timeFrom: "",
+            timeTo: "",
             days: [],
             travelFrom: [],
             travelTo: [],
@@ -89,32 +91,37 @@ const CreateRule = () => {
         console.log("submit");
         if (
             formState.id &&
+            formState.p_id &&
             formState.name &&
-            formState.context &&
-            formState.address &&
             formState.description &&
-            formState.email &&
-            formState.phone &&
-            formState.password &&
-            formState.password2
+            formState.context &&
+            formState.priority &&
+            formState.startDate &&
+            formState.days.length !== 0
         ) {
     
             const userData = {...formState};
             console.log(userData);
-            axios.post('http://localhost:5004/signup', userData).then( (res) => {
+            axios.post('http://localhost:5004/createRule', userData).then( (res) => {
                 setMsg('User Created Successfully.');
                 setFormState(
                     {
                         id: "",
+                        p_id: "",
                         name: "",
-                        context: "",
-                        rule_issuer: false,
                         description: "",
-                        email: "",
-                        address: "",
-                        phone: "",
-                        password: "",
-                        password2: ""
+                        context: "",
+                        priority: "",
+                        startDate: "",
+                        endDate: "",
+                        minAge: "",
+                        maxAge: "",
+                        timeFrom: "",
+                        timeTo: "",
+                        days: [],
+                        travelFrom: [],
+                        travelTo: [],
+                        occupationDeny: []
                     }
                 );
                 setLoading(false);
@@ -124,7 +131,7 @@ const CreateRule = () => {
                 setLoading(false);
             })
         } else {
-            setMsg("All fields are required");
+            setMsg("One or more required fields are missing");
             setLoading(false);
         }
     }
@@ -177,13 +184,13 @@ const CreateRule = () => {
                     <input value={formState.id} type="number" onChange={e => handleChange(e)} id="id" name="id" placeholder="id" />
                     <label htmlFor="p_id">P_id</label>
                     <input value={formState.p_id} type="number" onChange={e => handleChange(e)} id="id" name="p_id" placeholder="p_id" />
-                    <label htmlFor="name">Rule Label</label>
+                    <label htmlFor="name">*Rule Label</label>
                     <input value={formState.name} onChange={e => handleChange(e)} id="name" name="name" placeholder="e.g. Health Ministry" />
-                    <label htmlFor="description">Description</label>
+                    <label htmlFor="description">*Description</label>
                     <input value={formState.description} onChange={e => handleChange(e)} id="description" name="description" placeholder="Institute description"/>
                     <div className={styles.optionContainer}>
                         <div className={styles.inner}>
-                            <label htmlFor="context">Context</label>
+                            <label htmlFor="context">*Context</label>
                             <select onChange={e => handleChange(e)} id="context" name="context" value={formState.context}>
                                 <option value="">&lt;choose&gt;</option>
                                 <option value="health">Health</option>
@@ -193,7 +200,7 @@ const CreateRule = () => {
                             </select>
                         </div>
                         <div className={styles.inner}>
-                        <label htmlFor="priority">Priority</label>
+                        <label htmlFor="priority">*Priority</label>
                             <select onChange={e => handleChange(e)} id="priority" name="priority" value={formState.priority}>
                                 <option value="">&lt;choose&gt;</option>
                                 <option value="1">1</option>
@@ -204,16 +211,75 @@ const CreateRule = () => {
                             </select>   
                         </div>
                     </div>
-                    <label htmlFor="startDate">Applicable from</label>
+                    <label htmlFor="startDate">*Applicable from</label>
                     <input value={formState.startDate} type="date" onChange={e => handleChange(e)} id="startDate" name="startDate" placeholder="Date of birth" />
-                    <label htmlFor="endDate">Applicable till</label>
+                    <label htmlFor="endDate">Applicable till (leave blank if indefinite)</label>
                     <input value={formState.endDate} type="date" onChange={e => handleChange(e)} id="endDate" name="endDate" placeholder="Date of birth" />
-                    <label>Apply this rule for ages: </label>
+                    <label>Apply rule between these hours<br></br>(Leave blank to apply rule throughout the day):</label>
+                    <div className={styles.timeContainer}>
+                        <label htmlFor="timeFrom" >From</label>
+                        <select name="timeFrom" onChange={e => handleChange(e)} id="timeFrom" value={formState.timeFrom}>
+                            <option value="">&lt;choose&gt;</option>
+                            <option value="00:00">00:00</option>
+                            <option value="01:00">01:00</option>
+                            <option value="02:00">02:00</option>
+                            <option value="03:00">03:00</option>
+                            <option value="04:00">04:00</option>
+                            <option value="05:00">05:00</option>
+                            <option value="06:00">06:00</option>
+                            <option value="07:00">07:00</option>
+                            <option value="08:00">08:00</option>
+                            <option value="09:00">09:00</option>
+                            <option value="10:00">10:00</option>
+                            <option value="11:00">11:00</option>
+                            <option value="12:00">12:00</option>
+                            <option value="13:00">13:00</option>
+                            <option value="14:00">14:00</option>
+                            <option value="15:00">15:00</option>
+                            <option value="16:00">16:00</option>
+                            <option value="17:00">17:00</option>
+                            <option value="18:00">18:00</option>
+                            <option value="19:00">19:00</option>
+                            <option value="20:00">20:00</option>
+                            <option value="21:00">21:00</option>
+                            <option value="22:00">22:00</option>
+                            <option value="23:00">23:00</option>
+                        </select>
+                        <label htmlFor="timeTo" >To</label>
+                        <select id="timeTo" name="timeTo" onChange={e => handleChange(e)} value={formState.timeTo}>
+                            <option value="">&lt;choose&gt;</option>
+                            <option value="00:00">00:00</option>
+                            <option value="01:00">01:00</option>
+                            <option value="02:00">02:00</option>
+                            <option value="03:00">03:00</option>
+                            <option value="04:00">04:00</option>
+                            <option value="05:00">05:00</option>
+                            <option value="06:00">06:00</option>
+                            <option value="07:00">07:00</option>
+                            <option value="08:00">08:00</option>
+                            <option value="09:00">09:00</option>
+                            <option value="10:00">10:00</option>
+                            <option value="11:00">11:00</option>
+                            <option value="12:00">12:00</option>
+                            <option value="13:00">13:00</option>
+                            <option value="14:00">14:00</option>
+                            <option value="15:00">15:00</option>
+                            <option value="16:00">16:00</option>
+                            <option value="17:00">17:00</option>
+                            <option value="18:00">18:00</option>
+                            <option value="19:00">19:00</option>
+                            <option value="20:00">20:00</option>
+                            <option value="21:00">21:00</option>
+                            <option value="22:00">22:00</option>
+                            <option value="23:00">23:00</option>
+                        </select>
+                    </div>
+                    <label>Apply this rule for ages<br></br> (Leave blank to apply to all) </label>
                     <div className={styles.ageContainer}>
                         <label htmlFor="minAge">less than: </label>
-                        <input value={formState.minAge} name="minAge" id="minAge" onChange={e => handleChange(e)} ></input>
+                        <input value={formState.minAge} name="minAge" id="minAge" type="number" onChange={e => handleChange(e)} ></input>
                         <label htmlFor="maxAge">greater than: </label>
-                        <input value={formState.maxAge} name="maxAge" id="maxAge" onChange={e => handleChange(e)} ></input>
+                        <input value={formState.maxAge} name="maxAge" id="maxAge" type="number" onChange={e => handleChange(e)} ></input>
                     </div>
                     <label htmlFor="days">Rule is applicable on these days of the week</label>
                     <Multiselect
