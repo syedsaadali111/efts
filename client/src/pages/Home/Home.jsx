80ffda35ad8968d999e9abd178073fce89164ab3
Minal Zaka <minalzaka@gmail.com>
2021-04-16 01:03:05 +0300

    profile page implemented

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React, { useContext } from 'react';
-import {Link, useHistory} from 'react-router-dom';
 import { UserContext } from '../../helpers/userContext';
+import { useHistory } from 'react-router-dom';
 import styles from './Home.module.css';
 
 const Home = () => {
+    const user = useContext(UserContext);
+    console.log(user);
 
     const history = useHistory();
 
@@ -12,14 +14,30 @@ const Home = () => {
         history.push('/login');
     }
 
-    const user = useContext(UserContext);
+    const calculateAge = (dob) => {
+        let age = Math.floor((new Date(Date.now()).getFullYear() - new Date(dob).getFullYear()));
+        return age;
+    }
+
 
     return (
-        <div className={styles.main}>
-            <h1>{user.name}</h1>
-            <Link to='/filiation'>Contact Tracing</Link>
-            <Link to='/generate'>Your EFTS codes</Link>
-            <button onClick={handleLogout}>Logout</button>
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <h1>Your Account</h1>
+            </div>
+            <div className={styles.main}>
+                <div className={styles.profile}>
+                    <p><span>TC Kimlik no: </span> {user.user.id}</p>
+                    <p><span>Name: </span> {user.user.fname}</p>
+                    <p><span>Surname: </span> {user.user.sname}</p>
+                    <p><span>Gender: </span> {user.user.gender === "M" ? "Male" : "Female"}</p>
+                    <p><span>Date of birth: </span> {user.user.dob}</p>
+                    <p><span>Age: </span> {calculateAge(user.user.dob)}</p>
+                    <p><span>Risk status: </span> Riskless</p>
+                    <p><span>Risk factor: </span> 15%</p>
+                    <button onClick={handleLogout}>Logout</button>
+                </div>
+            </div>
         </div>
     );
 }
